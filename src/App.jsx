dd8a345434f0e4e7fa0533f32786fc8fe7d25715
flaviare1s <flaviarster@gmail.com>
2024-07-03 07:50:38 -0300

    Instalei React, Tailwind, copiei os arquivos necessários e adicionei fontes e cores customizadas.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+      <main className='font-space'>
+        <Footer />
+      </main>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="text-xs text-center p-2">
+      Challenge by <a className="font-bold text-strong-cyan" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a className="font-bold text-strong-cyan" href="https://github.com/flaviare1s">Flávia Reis</a>.
+    </footer>
+  )
+}
+
+export default Footer
