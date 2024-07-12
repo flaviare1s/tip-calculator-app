d08ba1ae902c3aa1e4bb1de4d44bea2029b2c700
flaviare1s <flaviarster@gmail.com>
2024-07-12 10:07:26 -0300

    Terminei de estilizar o Componente Tips na versão mobile e ajustei o componente Input

--- src/components/CalculatorBody.jsx
+++ src/components/CalculatorBody.jsx
@@ -1,11 +1,14 @@
-import Bill from "./Bill"
+import Input from "./Input"
 import Tips from "./Tips"
+import dollarIcon from '../assets/icon-dollar.svg'
+import personIcon from '../assets/icon-person.svg'
 
 const CalculatorBody = () => {
   return (
     <section className="bg-white p-8 md:p-[45px] w-full sm:w-[80%] md:max-w-[920px] rounded-t-[25px] md:rounded-b-[25px]">
-      <Bill />
+      <Input title='Bill' icon={dollarIcon} placeholder='0' legend='Dollar Icon' />
       <Tips />
+      <Input title='Number of People' icon={personIcon} placeholder='0' legend='Person Icon' />
     </section>
   )
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,11 @@
+const Input = ({ title, icon, placeholder, legend }) => {
+  return (
+    <section className='relative flex flex-col'>
+      <h3 className="text-dark-grayish-cyan font-bold">{title}</h3>
+      <input className='bg-very-light-grayish-cyan h-12 text-very-dark-cyan font-bold text-2xl text-right px-5 rounded-[5px]' type="text" id="bill" name="bill" placeholder={placeholder} required/>
+      <img className="absolute top-[40px] left-[20px]" src={icon} alt={legend} />
+    </section>
+  )
+}
+
+export default Input
