9d679f98d040b17e1993891202e83d2ef806bcf0
flaviare1s <flaviarster@gmail.com>
2024-07-12 15:11:09 -0300

    Finalizando os estilos e ajustando o cálculo

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react"
+import './Input.css'
 
 const Input = ({ title, icon, placeholder, legend, onChange, min }) => {
   const [error, setError] = useState('')
@@ -18,14 +20,16 @@ const Input = ({ title, icon, placeholder, legend, onChange, min }) => {
     <section className='relative flex flex-col'>
       <h3 className="text-dark-grayish-cyan font-bold mb-1.5">{title}</h3>
       <input
-        className='bg-very-light-grayish-cyan h-12 text-very-dark-cyan font-bold text-2xl text-right px-5 rounded-[5px] text-[#00474B] placeholder:text-[#00474B]' 
+        className={`bg-very-light-grayish-cyan h-12 text-very-dark-cyan font-bold text-2xl text-right px-5 rounded-[5px] placeholder:text-[#00474B] border-2 border-transparent outline-none focus:border-strong-cyan ${error ? 'error' : ''}`}
         type="number"
-        defaultValue={placeholder}
+        value={placeholder}
         min={min}
         onChange={(e) => handleChange(e.target.value)}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
       />
       {error && <small className="text-[#E17457] text-base font-bold absolute top-0 right-0">{error}</small>}
-      <img className="absolute top-[40px] left-[20px]" src={icon} alt={legend} />
+      <img className="absolute top-[45px] left-[20px]" src={icon} alt={legend} />
     </section>
   )
 }
--- src/components/Tips.jsx
+++ src/components/Tips.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
-import './Tips.css';
-import { useState } from "react";
+import './Tips.css'
+import { useState } from "react"
 
 const Tips = ({ tip, onTipChange }) => {
-  const [customTip, setCustomTip] = useState('');
+  const [customTip, setCustomTip] = useState('')
 
   function handleTipClick(value) {
     onTipChange(value)
@@ -25,13 +25,13 @@ const Tips = ({ tip, onTipChange }) => {
     <section className="py-8">
       <h3 className="text-dark-grayish-cyan font-bold mb-[16px]">Select Tip %</h3>
       <div className="tips grid grid-cols-2 md:grid-cols-3 gap-4 max-w-[311px] md:max-w-[379px] m-auto">
-        <button className={tip === 5 ? 'selected' : 'unselected'} onClick={() => handleTipClick(5)}>5%</button>
-        <button className={tip === 10 ? 'selected' : 'unselected'} onClick={() => handleTipClick(10)}>10%</button>
-        <button className={tip === 15 ? 'selected' : 'unselected'} onClick={() => handleTipClick(15)}>15%</button>
-        <button className={tip === 25 ? 'selected' : 'unselected'} onClick={() => handleTipClick(25)}>25%</button>
-        <button className={tip === 50 ? 'selected' : 'unselected'} onClick={() => handleTipClick(50)}>50%</button>
+        <button className={`${tip === 5 ? 'selected' : 'unselected'} hover:bg-[#9FE8DF] hover:text-[#00474B]`} onClick={() => handleTipClick(5)}>5%</button>
+        <button className={`${tip === 10 ? 'selected' : 'unselected'} hover:bg-[#9FE8DF] hover:text-[#00474B]`} onClick={() => handleTipClick(10)}>10%</button>
+        <button className={`${tip === 15 ? 'selected' : 'unselected'} hover:bg-[#9FE8DF] hover:text-[#00474B]`} onClick={() => handleTipClick(15)}>15%</button>
+        <button className={`${tip === 25 ? 'selected' : 'unselected'} hover:bg-[#9FE8DF] hover:text-[#00474B]`} onClick={() => handleTipClick(25)}>25%</button>
+        <button className={`${tip === 50 ? 'selected' : 'unselected'} hover:bg-[#9FE8DF] hover:text-[#00474B]`} onClick={() => handleTipClick(50)}>50%</button>
         <input
-          className='bg-[#F3F9FA] text-[#547878] placeholder:text-[#547878] font-bold text-2xl text-right pr-3'
+          className='bg-[#F3F9FA] text-[#547878] placeholder:text-[#547878] font-bold text-2xl text-right pr-3 border-2 border-transparent outline-none focus:border-strong-cyan rounded-[5px]'
           type="number"
           placeholder='Custom'
           value={customTip}
@@ -42,4 +42,4 @@ const Tips = ({ tip, onTipChange }) => {
   )
 }
 
-export default Tips;
+export default Tips
