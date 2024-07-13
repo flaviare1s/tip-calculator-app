7e2a5021512fc7255ebbc1d580e3903d172e8f42
flaviare1s <flaviarster@gmail.com>
2024-07-13 01:06:36 -0300

    Ajustei padding no header da versão desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.svg'
 const Header = () => {
   return (
     <header>
-      <img className='pt-[50px] pb-10' src={logo} alt="Logo" />
+      <img className='pt-[50px] pb-10 md:pb-[88px]' src={logo} alt="Logo" />
     </header>
   )
 }
